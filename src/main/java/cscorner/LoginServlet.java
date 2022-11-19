8273a27121738483cc3ebf4eea11632ef44beeeb
Eddy Guo <k1219851@gmail.com>
2022-11-19 13:08:44 -0600

    inital web app

---
+++ src/main/java/cscorner/LoginServlet.java
@@ -0,0 +1,21 @@
+package cscorner;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
